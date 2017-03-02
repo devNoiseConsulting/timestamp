1df911cc85a03c0e19f2d53a6412b5334cfc82a2
Michael Flynn <flynnmj@spamMini.local>
2017-03-02 16:31:36 -0500

    Working on a Heroku setup.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 8080));
+
 app.get('/:id', function(req, res) {
   var output = getTheTime(req.params.id);
   res.end(JSON.stringify(output));
@@ -10,8 +12,8 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function() {
-  console.log('Example app listening on port 8080!')
+app.listen(app.get('port'), function() {
+  console.log('Timestamp microservice is listening on port ', app.get('port'));
 });
 
 function getTheTime(timestamp) {
